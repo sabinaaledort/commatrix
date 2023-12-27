f298ea300944bf52148013b2071d11812fc33eb5
Lior Noy <lnoy@redhat.com>
2023-12-27 14:40:28 +0200

    Add golangci linter check to CI/CD
    This commit adds a new step in the CI/CD process,
    incorporating the golangci linter to analyze the repository.
    
    As part of this update, the Makefile is eliminated
    and we transition to using tasks.py.
    
    Signed-off-by: Lior Noy <lnoy@redhat.com>

---
+++ tasks.py
@@ -0,0 +1,28 @@
+from invoke import run, task
+
+@task
+def test(ctx):
+    """Run unit tests."""
+    run("go test ./internal/...")
+
+@task(help={
+    "env": "Specify in which environment to run the linter . Default 'container'. Supported: 'container','host'"
+})
+def lint(ctx, env="container"):
+    """Run linter.
+
+    By default, this will run a golangci-lint docker image against the code.
+    However, in some environments (such as the MetalLB CI), it may be more
+    convenient to install the golangci-lint binaries on the host. This can be
+    achieved by running `inv lint --env host`.
+    """
+    version = "1.55.2"
+    golangci_cmd = "golangci-lint run --timeout 10m0s ./..."
+
+    if env == "container":
+        run("docker run --rm -v $(git rev-parse --show-toplevel):/app -w /app golangci/golangci-lint:v{} {}".format(version, golangci_cmd), echo=True)
+    elif env == "host":
+        run("curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v{}".format(version))
+        run(golangci_cmd)
+    else:
+        raise Exit(message="Unsupported linter environment: {}". format(env))
