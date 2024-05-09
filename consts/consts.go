d534e35c855a5e01d0856e1d6d6982827b828e51
Sabina Aledort <saledort@redhat.com>
2024-05-09 10:17:24 +0300

    Update debug pod image to a more generic image

--- consts/consts.go
+++ consts/consts.go
@@ -7,6 +7,5 @@ const (
 	OptionalTrue          = "true"
 	RoleLabel             = "node-role.kubernetes.io/"
 	DefaultDebugNamespace = "openshift-commatrix-debug"
-	// TODO: change the image.
-	DefaultDebugPodImage = "quay.io/openshift-release-dev/ocp-v4.0-art-dev@sha256:535ce24b5f1894d2a07bfa7eed7ad028ffde0659693f2a571ac4712a21cd028c"
+	DefaultDebugPodImage  = "quay.io/openshift-release-dev/ocp-release:4.15.12-multi"
 )
